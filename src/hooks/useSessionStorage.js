847a056753983898c4cb2eb021d63afd4823f94a
Jose Valentin Salina <josevalentinsp@gmail.com>
2022-07-18 21:29:17 -0400

    using token

--- src/hooks/useSessionStorage.js
+++ src/hooks/useSessionStorage.js
@@ -3,7 +3,7 @@ export function useSessionStorage (key, initialValue) {
   const [storedValue, setValue] = useState(() => {
     try {
       const item = window.sessionStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
+      return item || initialValue
     } catch (err) {
       return initialValue
     }
@@ -11,7 +11,7 @@ export function useSessionStorage (key, initialValue) {
 
   const setSessionStorage = value => {
     try {
-      window.sessionStorage.setItem(key, JSON.stringify(value))
+      window.sessionStorage.setItem(key, value)
       setValue(value)
     } catch (err) {
       console.error(err)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { App } from './app'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink, from } from '@apollo/client'
 import { AppProvider } from './hooks/useContext'
+import { onError } from '@apollo/client/link/error'
+import { setContext } from '@apollo/client/link/context'
+
+const authMiddleware = setContext((_, { headers }) => {
+  const token = window.sessionStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'https://petgram-server-valentin.vercel.app/graphql'
+})
+
+const errorMiddleware = onError(
+  ({ networkError }) => {
+    if (networkError && networkError.response.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location = '/user'
+    }
+  }
+)
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-valentin.vercel.app/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    httpLink
+  ])
 })
 
 ReactDom.render(
